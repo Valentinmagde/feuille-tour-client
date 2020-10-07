978fc586a0053cf79dec856ee90441afd2c0f95b
armelbouri <moudinearmel@gmail.com>
2020-10-07 12:48:09 +0200

    crud categorie

--- js/chefboutique/categorie.js
+++ js/chefboutique/categorie.js
@@ -73,36 +73,26 @@ function modifierPompe(k) {
                 var pompe = document.getElementById('pompe_' + k)
 
                 pompe.cells[1].innerText = a
-                pompe.cells[2].innerText = b
-                pompe.cells[3].innerText = x
-                pompe.cells[4].innerText = y
-                pompe.cells[4].innerText = z
-                pompe.cells[6].innerText = uneStation(r)
-                swal("Bon travail!", "Pompe modifié avec succès!", "success");
+                swal("Bon travail!", "categorie modifiée avec succès!", "success");
             } else {
-                swal("Mauvais travail!", "Modification échouée, recommencez!", "error");
+                swal("Erreur!", "Modification échouée, recommencez!", "error");
             }
         }
     };
 
     var a = document.getElementById('nom' + k + '').value;
-    var b = document.getElementById('prix' + k + '').value;
-    var x = document.getElementById('typevolucompteur' + k + '').value;
-    var y = document.getElementById('idexdebut' + k + '').value;
-    var z = document.getElementById('idexfin' + k + '').value;
-    var t = document.getElementById('station' + k + '').value;
+    
 
-    var parameters = "method=modif&nom=" + a + "&prix=" + b +"&typevolucompteur=" + x + "&indexdebut=" + y + 
-    "&indexfin=" + z + "&station=" + t + "&id=" + k;
+    var parameters = "method=modif&nom="+a+"&id=" + k;
     //var parameters="limit=5";
-    xhttp.open("POST", "http://" + localStorage.getItem("cam") + "/asa/pompes.php", true);
+    xhttp.open("POST", "http://" + localStorage.getItem("cam") + "/asa/categories.php", true);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhttp.send(parameters);
 }
 
 //-------Afficher la liste des actions--------
-function afficheListePompes() {
-    chargerTb(4)
+function afficheListeCat() {
+    chargerTb(7)
         .then((res) => {
             var arr = JSON.parse(res);
             var out = "";
@@ -110,12 +100,7 @@ function afficheListePompes() {
             for (i = 0; i < arr.length; i++) {
                 out = '<tr id="pompe_' + arr[i].id + '">' +
                     '<th scope="row">' + arr[i].id + '</th>' +
-                    '<td>' + arr[i].nom + '</td>' +
-                    '<td>' + arr[i].prix + '</td>' +
-                    '<td>' + arr[i].type_volucompteur + '</td>' +
-                    '<td>' + arr[i].index_debut + '</td>' +
-                    '<td>' + arr[i].index_fin + '</td>' +
-                    '<td id="' + arr[i].id_station + '">' + uneStation(arr[i].id_station) + '</td>' +
+                    '<td>' + arr[i].designation + '</td>' +
                     '<td>' +
                     '<div class="btn-group btn-group-xs dropup">' +
                     '<button type="button" class="btn btn-info btn-pretty dropdown-toggle" data-toggle="dropdown">' +
@@ -130,7 +115,7 @@ function afficheListePompes() {
                     '<div class="modal-content">' +
                     '<div class="modal-header">' +
                     '<button type="button" class="close" data-dismiss="modal">&times;</button>' +
-                    '<h4 class="modal-title">Supprimer ' + arr[i].nom + '?</h4>' +
+                    '<h4 class="modal-title">Supprimer ' + arr[i].designation + '?</h4>' +
                     '</div>' +
                     '<div class="modal-body">' +
                     '<p><button type="button" class="btn btn-warning btn-lg" onclick="supprimerPompe(' + arr[i].id + ')">Supprimer</button>' +
@@ -161,69 +146,7 @@ function afficheListePompes() {
                     '<label for="recipient-name" class="col-form-label">Nom :</label>' +
                     '</div>' +
                     '<div class="col-sm-9">' +
-                    '<input type="text" class="form-control" id="nom' + arr[i].id + '" value=' + arr[i].nom + ' style="width: 100%">' +
-                    '</div>' +
-                    '</div>' +
-                    '</div>' +
-                    '<br>' +
-
-                    '<div class="row">' +
-                    '<div class="form-group col-md-12">' +
-                    '<div class="col-sm-3">' +
-                    '<label for="recipient-name" class="col-form-label">Prix :</label>' +
-                    '</div>' +
-                    '<div class="col-sm-9">' +
-                    '<input type="number" class="form-control" id="prix' + arr[i].id + '" value=' + arr[i].prix + ' style="width: 100%">' +
-                    '</div>' +
-                    '</div>' +
-                    '</div>' +
-                    '<br>' +
-
-                    '<div class="row">' +
-                    '<div class="form-group col-md-12">' +
-                    '<div class="col-sm-3">' +
-                    '<label for="recipient-name" class="col-form-label">Type volucompteur :</label>' +
-                    '</div>' +
-                    '<div class="col-sm-9">' +
-                    '<input type="text" class="form-control" id="typevolucompteur' + arr[i].id + '" value=' + arr[i].type_volucompteur + ' style="width: 100%">' +
-                    '</div>' +
-                    '</div>' +
-                    '</div>' +
-                    '<br>' +
-
-                    '<div class="row">' +
-                    '<div class="form-group col-md-12">' +
-                    '<div class="col-sm-3">' +
-                    '<label for="recipient-name" class="col-form-label"> Index début:</label>' +
-                    '</div>' +
-                    '<div class="col-sm-9">' +
-                    '<input type="number" class="form-control" id="idexdebut' + arr[i].id + '" value=' + arr[i].index_debut + ' style="width: 100%">' +
-                    '</div>' +
-                    '</div>' +
-                    '</div>' +
-                    '<br>' +
-
-                    '<div class="row">' +
-                    '<div class="form-group col-md-12">' +
-                    '<div class="col-sm-3">' +
-                    '<label for="recipient-name" class="col-form-label"> Index fin:</label>' +
-                    '</div>' +
-                    '<div class="col-sm-9">' +
-                    '<input type="number" class="form-control" id="idexfin' + arr[i].id + '" value=' + arr[i].index_fin + ' style="width: 100%">' +
-                    '</div>' +
-                    '</div>' +
-                    '</div>' +
-                    '<br>' +
-
-                    '<div class="row">' +
-                    '<div class="form-group col-md-12">' +
-                    '<div class="col-md-3">' +
-                    '<label for="message-text" class="col-form-label">Station :</label>' +
-                    '</div>' +
-                    '<div class="col-md-9">' +
-                    '<select onkeyup="verificationVide()" class="form-control" id="station' + arr[i].id + '" style="width: 100%">' +
-                    '<option id="' + arr[i].id + '" value=' + arr[i].id_station + '>' + uneStation(arr[i].id_station) + '</option>' +
-                    '</select>' +
+                    '<input type="text" class="form-control" id="nom' + arr[i].id + '" value=' + arr[i].designation + ' style="width: 100%">' +
                     '</div>' +
                     '</div>' +
                     '</div>' +
@@ -240,13 +163,12 @@ function afficheListePompes() {
                     '</td>' +
                     '</td>' +
                     '</tr>';
-                //alert(out);
-                $('#listesdespompes').append(out);
+                $('#listecat').append(out);
             }
             initPage();
         })
         .catch((error) => {
-            console.error(error)
+            console.log(error)
         })
 
 }
@@ -272,8 +194,7 @@ function enregistrerUneCat() {
 
     var nom = document.getElementById("nom").value;
     
-
-    var parameters = "method=creer&desigation=" + nom;
+    var parameters = "method=creer&desigation="+nom;
     //var parameters="limit=5";
     xhttp.open("POST", "http://" + localStorage.getItem("cam") + "/asa/categories.php", true);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
